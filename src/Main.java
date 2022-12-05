bf50cc0f2ace2db6535d3e0e7f9925074491b6ec
ablirysa <ysabellea@nycstudents.net>
2022-12-04 21:51:39 -0500

    toString() needed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to off-brand Wordle!");
+        System.out.print("What's your name? ");
+        String name = scan.nextLine();
+
+        System.out.print("So, " + name + ", do you want to know how to play (yes/no)? ");
+        String choice = scan.nextLine();
+        if (choice.equals("yes")) {
+            String instructions = "The only things you need to understand are the levels, the meaning of the colors, and the only rule.";
+            instructions += "\nCOLORS: \n     - Green = The right letter and in the right place \n     - Yellow = The right letter, but in the wrong place \n     - Grey = Not in the word";
+            instructions += "\nLEVELS: \n     - 1 = Four letter words \n     - 2 = Five letter words \n     - 3 = Six letter words";
+            instructions += "\nRULE: \n     - DON'T use double letters, no word has two of the same letter";
+            System.out.println(instructions);
+        }
+        System.out.println();
+
+        System.out.print("Alright, so what mode do you want to do (1 to 3)? ");
+        int mode = scan.nextInt();
+        while (!(mode >= 1 && mode <= 3)) {
+            System.out.print("That wasn't in the range... Try again: ");
+            mode = scan.nextInt();
+        }
+        System.out.println();
+
+        Wordle game = new Wordle(name, mode);
+        game.wordRandomizer();
+
+        System.out.print("Are you ready to start, " + name + " (yes/no)? ");
+        String choice2 = scan.nextLine();
+        while (!(choice2.equals("yes"))) {
+            System.out.print("Are you ready to start, " + name + " (yes/no)? ");
+            choice2 = scan.nextLine();
+        }
+        System.out.println();
+        game.start();
+
+        System.out.print("Guess: ");
+        String guess = scan.nextLine();
+        game.plusGuess();
+        while (!game.check(guess)) {
+            game.plusGuess();
+            System.out.println(game.letterChecker(guess));
+            System.out.print("Guess: ");
+            guess = scan.nextLine();
+        }
+        game.end();
     }
 }
--- src/Wordle.java
+++ src/Wordle.java
@@ -1,2 +1,113 @@
-package PACKAGE_NAME;public class Wordle {
+/**
+ * The Wordle class represents a knock-off version of Wordle where the user guesses the word and is informed about the letters.
+ */
+public class Wordle {
+    /** This is the player's name. */
+    private String name;
+    /** This is the answer the user will be attempting to guess. */
+    private String answer;
+    /** This is the level the user prefers to play. */
+    private String level;
+    /** This is the amount of guesses the user has made. */
+    private int guesses;
+    /** This will assist in stopping the color change. */
+    private static final String RESET = "\u001B[0m";
+    /** This will assist in identifying if the letter is correct. */
+    private static final String GREEN = "\u001B[32m";
+    /** This will assist in identifying if the letter is correct, but in the wrong place. */
+    private static final String YELLOW = "\u001B[33m";
+
+    /** A two-parameter constructor that initializes the name and level.
+     * @param name - User's preferred name
+     * @param level - User's preferred level
+     */
+    public Wordle(String name, int level) {
+        this.name = name;
+        if (level == 1) {
+            this.level = "EASY";
+        } else if (level == 2) {
+            this.level = "MEDIUM";
+        } else {
+            this.level = "HARD";
+        }
+    }
+
+    /** Zero-parameter method that selects the answer by chance (three options per level). */
+    public void wordRandomizer() {
+        int num = (int) (Math.random() * 3) + 1;
+        if (level.equals("EASY")) { // 4-letter words
+            if (num == 1) {
+                answer = "bunt";
+            } else if (num == 2) {
+                answer = "trap";
+            } else {
+                answer = "acid";
+            }
+        } else if (level.equals("MEDIUM")) { // 5-letter words
+            if (num == 1) {
+                answer = "viper";
+            } else if (num == 2) {
+                answer = "study";
+            } else {
+                answer = "nacho";
+            }
+        } else { // 6-letter words
+            if (num == 1) {
+                answer = "throne";
+            } else if (num == 2) {
+                answer = "answer";
+            } else {
+                answer = "volume";
+            }
+        }
+    }
+
+    /** Zero-parameter method that prints out the header for the start of the game. */
+    public void start() {
+        System.out.println("- - - " + level + " MODE - - -");
+    }
+
+    /** One-parameter method that evaluates if the user's guess is the answer.
+     * @param guess - User's attempt
+     * @return boolean - Only true if the user's guess is the answer.
+     */
+    public boolean check(String guess) {
+        guess = guess.toLowerCase();
+        if (guess.equals(answer)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /** Zero-parameter method that increases the amount of guesses taken, for the purpose of stats. */
+    public void plusGuess() {
+        guesses++;
+    }
+
+    public String letterChecker(String guess) {
+        String corrections = "";
+        for (int i = 0; i < guess.length(); i++) {
+            String letter = guess.substring(i, i + 1);
+            if (answer.indexOf(letter) == -1) {
+                corrections += letter;
+            } else if (answer.indexOf(letter) == i) {
+                corrections += GREEN + letter + RESET;
+            } else {
+                corrections += YELLOW + letter + RESET;
+            }
+        }
+        return corrections;
+    }
+
+    /** Zero-parameter method that prints out the header and game information for the end of the game. */
+    public void end() {
+        String end = "- - - - - - - - - - - -";
+        end += "\nGreat job, " + name + "!";
+        end += "\nGuesses Taken: " + guesses;
+        end += "\nCorrect Answer: " + answer;
+        System.out.println(end);
+    }
+
+    // NEED toString()
 }
